293920911d0f9aa8e713d5bd1d7d3f82c2de30d7
Иван <>
2017-05-12 11:48:41 +0300

    последние правки(надеюсь:з)

--- js/script.js
+++ js/script.js
@@ -1,47 +1,30 @@
-  var link = document.querySelector(".search-btn");
-  var popup = document.querySelector(".wrapper-form");
-  var visit = popup.querySelector("[name=visit]");
-  var departure = popup.querySelector("[name=departure]");
-  var people = popup.querySelector("[name=people]");
-  var child = popup.querySelector("[name=child]");
-  var form = popup.querySelector("[name=search]")
-  var storageVisit = localStorage.getItem("visit");
-  var storageDeparture = localStorage.getItem("departure");
-  var storagePeople = localStorage.getItem("people");
-  var storageChild = localStorage.getItem("child");
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".wrapper-form");
+var visit = popup.querySelector("[name=visit]");
+var departure = popup.querySelector("[name=departure]");
+var people = popup.querySelector("[name=people]");
+var child = popup.querySelector("[name=child]");
+var form = popup.querySelector("[name=search]")
 
-  popup.classList.add("wrapper-form-hide");
-  link.addEventListener("click", function(event) {
+popup.classList.add("wrapper-form-hide");
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.toggle("wrapper-form-hide");
+  visit.focus();
+});
+
+form.addEventListener("submit", function(event) {
+  if (!visit.value || !departure.value || !people.value|| !child.value) {
     event.preventDefault();
-    popup.classList.toggle("wrapper-form-hide");
-    if (storageVisit) {
-      visit.value = storageVisit;
-      departure.focus();
-    } else {
-      visit.focus();
-    }
-  });
+    popup.classList.remove("wrapper-form-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("wrapper-form-error");
+  }
+});
 
-  form.addEventListener("submit", function(event) {
-    if (!visit.value || !departure.value || !people.value|| !child.value) {
-      event.preventDefault();
-      popup.classList.remove("wrapper-form-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("wrapper-form-error");
-    } else {
-     localStorage.setItem("visit", visit.value);
-     localStorage.setItem("departure", departure.value);
-     localStorage.setItem("people", people.value);
-     localStorage.setItem("child", child.value);
-    }
-  });
-
-  window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-      if (popup.classList.contains("wrapper-form-hide")) 
-      else  {
-        popup.classList.add("wrapper-form-hide");
-        popup.classList.remove("wrapper-form-error");
-      }
-    }
- });
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    popup.classList.add("wrapper-form-hide");
+    popup.classList.remove("wrapper-form-error");
+  }
+});
